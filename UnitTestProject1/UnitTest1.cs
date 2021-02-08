8334832adeaacedfe7e20f09bc6abd6a629bb87b
vapaus <snaqerou@gmail.com>
2021-02-09 00:32:37 +0100

    Tests

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        // Chciałem napisać testy, ale visual studio nie może dodać mi referencji.. Próbowałem na różne sposoby
+
+        //[TestMethod]
+        //[DataRow("[value, 123]", "value")]
+        //[DataRow("[123, value]", "123")]
+        //public void GetIdValue_Ok(string value, string result)
+        //{
+        //    var valueFromStrin = GetIdValueUtil.GetIdValue(value);
+
+        //    Assert.AreEqual(valueFromStrin, result);
+        //}
+    }
+}
